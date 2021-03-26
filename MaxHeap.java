06e95e3374cf321a63fce1aca5dbec0a2a772ac4
Joseph <46443751+TheMusket@users.noreply.github.com>
2021-03-25 20:05:40 -0700

    Added method reheap to MaxHeap.

--- MaxHeap.java
+++ MaxHeap.java
@@ -94,4 +94,37 @@ public class MaxHeap<T extends Comparable<? super T>>
 		
 		return root;		
 	}
+	
+	private void reheap(int rootIndex)
+	{
+		boolean done = false;
+		T orphan = heap[rootIndex];
+		int leftChildIndex = 2 * rootIndex;
+		
+		while (!done && (leftChildIndex <= lastIndex))
+		{
+			int largerChildIndex = leftChildIndex; // Assume larger
+			int rightChildIndex = leftChildIndex + 1;
+			
+			if ((rightChildIndex <= lastIndex) && 
+					heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0)
+			{
+				largerChildIndex = rightChildIndex;
+			}
+			
+			if (orphan.compareTo(heap[largerChildIndex]) < 0)
+			{
+				heap[rootIndex] = heap[largerChildIndex];
+				rootIndex = largerChildIndex;
+				leftChildIndex = 2 * rootIndex;
+			}
+			
+			else 
+			{
+				done = true;
+			}
+		}
+		
+		heap[rootIndex] = orphan;
+	}
 }
